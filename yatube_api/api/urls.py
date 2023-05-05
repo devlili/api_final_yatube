428decd2d8554703dacc48c6dbe87717c7f7684b
Лилия Альмухаметова <almuhametova@gmail.com>
2023-05-05 23:57:17 +0500

    Change path

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,15 +6,15 @@ from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 app_name = "api"
 
 router_v1 = routers.DefaultRouter()
-router_v1.register(r"v1/posts", PostViewSet)
-router_v1.register(r"v1/groups", GroupViewSet)
-router_v1.register(r"v1/follow", FollowViewSet, basename="follow")
+router_v1.register(r"posts", PostViewSet)
+router_v1.register(r"groups", GroupViewSet)
+router_v1.register(r"follow", FollowViewSet, basename="follow")
 router_v1.register(
-    r"v1/posts/(?P<post_id>\d+)/comments", CommentViewSet, basename="comments"
+    r"posts/(?P<post_id>\d+)/comments", CommentViewSet, basename="comments"
 )
 
 urlpatterns = [
-    path("", include(router_v1.urls)),
+    path("v1/", include(router_v1.urls)),
     path("v1/", include("djoser.urls")),
     path("v1/", include("djoser.urls.jwt")),
 ]
